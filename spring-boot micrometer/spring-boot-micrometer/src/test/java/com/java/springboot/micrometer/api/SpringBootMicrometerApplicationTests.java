844d2d05707bf599f8eea2a4cebeea82e1f76948
NainsiU <150132781+NainsiU@users.noreply.github.com>
2023-11-08 16:05:33 +0530

    Add files via upload

---
+++ spring-boot micrometer/spring-boot-micrometer/src/test/java/com/java/springboot/micrometer/api/SpringBootMicrometerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.java.springboot.micrometer.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBootMicrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
